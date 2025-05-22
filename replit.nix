364627e0b822afd13820a20a6c1c3711a1d7de8a
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2025-05-22 13:51:02 +0000

    Pre-Recovery checkpoint

--- replit.nix
+++ replit.nix
@@ -1,7 +1,7 @@
 { pkgs }: {
 	deps = [
-		pkgs.nodejs
-        pkgs.nodePackages.typescript-language-server
-        pkgs.yarn
+		pkgs.nodejs-20_17
+		pkgs.nodePackages.typescript-language-server
+		pkgs.yarn
 	];
 }
